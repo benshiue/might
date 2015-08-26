1c42aa95c1e4b97020ea03379a1ef22a8b52401b
BenShiue <benshiue@orangicetech.com>
2015-08-26 22:04:19 +0800

    Fix img rwd bug

--- assets/js/loop.js
+++ assets/js/loop.js
@@ -77,8 +77,7 @@ jQuery( function($){
 			thumb_links: 0,				// Individual thumb links for each slide
 			slides:  	[				// Slideshow Images
 							
-							{image : 'assets/img/sliders/slider2.png', title : '<div class="hero-text"><h2 class="hero-heading">MIGHT GROUP for Start-up</h2><p>To Be Your Best EMS Partners</p></div>', thumb : '', url : ''},
-							
+							{image : 'assets/img/sliders/slider2.png', title : '<div class="hero-text"><h2 class="hero-heading">MIGHT GROUP for Start-up</h2><p>To Be Your Best EMS Partners</p></div>', thumb : '', url : ''}  
 						],
 		});
 
